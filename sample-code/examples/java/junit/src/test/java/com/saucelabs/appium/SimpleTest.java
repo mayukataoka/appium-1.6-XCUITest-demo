628d6d35a3631a774a5d2105e060bd2489ec36b6
mayukataoka <mayu.kataoka@gmail.com>
2016-12-04 00:40:42 -0800

    Initial commit

---
+++ sample-code/examples/java/junit/src/test/java/com/saucelabs/appium/SimpleTest.java
@@ -0,0 +1,91 @@
+package com.saucelabs.appium;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.ios.IOSDriver;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SimpleTest {
+
+    private AppiumDriver<WebElement> driver;
+
+    private List<Integer> values;
+
+    private static final int MINIMUM = 0;
+    private static final int MAXIMUM = 10;
+
+    @Before
+    public void setUp() throws Exception {
+        // set up appium
+        File appDir = new File(System.getProperty("user.dir"), "../../../apps/TestApp/build/release-iphonesimulator");
+        File app = new File(appDir, "TestApp.app");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformVersion", "10.1");
+        capabilities.setCapability("deviceName", "iPhone 7 Plus");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver = new IOSDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        values = new ArrayList<Integer>();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    private void populate() {
+        //populate text fields with two random number
+        Random random = new Random();
+        int rndNum1 = random.nextInt(MAXIMUM - MINIMUM + 1) + MINIMUM;
+        values.add(rndNum1);
+        int rndNum2 = random.nextInt(MAXIMUM - MINIMUM + 1) + MINIMUM;
+        values.add(rndNum2);
+
+        driver.findElement(By.name("IntegerA")).sendKeys(String.valueOf(rndNum1));
+        driver.findElement(By.name("IntegerB")).sendKeys(String.valueOf(rndNum2));
+    }
+
+
+    @Test
+    public void testUIComputation() throws Exception {
+
+        driver.switchTo().alert().accept();
+        Thread.sleep(3000); //Due to the slowness of closing Alerts, I needed to add a sleep here.
+        // populate text fields with values
+        populate();
+        // trigger computation by using the button
+        WebElement button = driver.findElement(By.className("XCUIElementTypeButton"));
+        button.click();
+        // is sum equal ?
+        WebElement texts = driver.findElement(By.name("Answer"));
+        assertEquals(String.valueOf(values.get(0) + values.get(1)), texts.getText());
+    }
+
+}
